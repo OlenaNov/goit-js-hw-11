3c795ddd79c123914fb0136c206aebfea5f6a8d1
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-02-26 22:53:19 +0100

    simplelightbox-err

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 import markupImage from './markup-image-card';
 import getImages from './fetchImages';
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const form = document.querySelector('#search-form');
 const btnLoadMore = document.querySelector('.load-more');
-const boxGallery = document.querySelector('.gallery');
+const gallery = document.querySelector('.gallery');
 
 btnLoadMore.style.display = "none";
 
@@ -18,7 +21,7 @@ function onSubmit(e) {
     e.preventDefault();
     queryValue = form.elements.searchQuery.value;
     console.log(queryValue);
-    boxGallery.innerHTML = "";
+    gallery.innerHTML = "";
     currentPage = 1;
     makeFetch();
 };
@@ -26,9 +29,9 @@ function onSubmit(e) {
 async function makeFetch() {
     try {
 
-        btnLoadMore.style.display = "block";
         const fetchResult = await getImages(queryValue, currentPage);
-        markupImage(fetchResult.hits, boxGallery);
+        markupImage(fetchResult.hits, gallery);
+        btnLoadMore.style.display = "block";
 
         if(currentPage === 1) {
             Notify.success(`Hooray! We found ${fetchResult.totalHits} images.`);
@@ -44,9 +47,16 @@ async function makeFetch() {
     }
 };
 
+
+const lightbox = new SimpleLightbox('.gallery a', { 
+
+    animationSpeed:	300,
+  });
+
 btnLoadMore.addEventListener('click', onBtnLoadMore);
 
 function onBtnLoadMore() {
     currentPage += 1;
     makeFetch();
-}
+    lightbox.refresh();
+};
--- src/markup-image-card.js
+++ src/markup-image-card.js
@@ -1,25 +1,28 @@
 
 export default async function markupImage(arrImages, element) {
 
-    const markap = arrImages.map(({ webformatURL, tags, likes, views, comments, downloads }) => 
-    `<div class="photo-card">
-        <img src="${webformatURL}" alt="${tags}" loading="lazy" width=200px"/>
-        <div class="info">
-          <p class="info-item">
-            <b>Likes</b> ${likes}
-          </p>
-          <p class="info-item">
-            <b>Views</b> ${views}
-          </p>
-          <p class="info-item">
-            <b>Comments</b> ${comments}
-          </p>
-          <p class="info-item">
-            <b>Downloads</b> ${downloads}
-          </p>
-        </div>
-    </div>`
+    const markap = arrImages.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => 
+    `<a href="${largeImageURL}" class="gallery__item">
+    <img src="${webformatURL}" alt="${tags}" loading="lazy" class="gallery__image"/>
+    <div class="info">
+      <p class="info-item">
+        <b>Likes</b>
+        ${likes}
+      </p>
+      <p class="info-item">
+        <b>Views</b>
+        ${views}
+      </p>
+      <p class="info-item">
+        <b>Comments</b>
+        ${comments}
+      </p>
+      <p class="info-item">
+        <b>Downloads</b>
+        ${downloads}
+      </p>
+    </div></a>`
     ).join('');
 
-    element.insertAdjacentHTML("beforeend" ,markap);
+    element.insertAdjacentHTML("beforeend", markap);
 };
