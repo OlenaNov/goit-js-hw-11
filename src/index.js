4018cb452ecb8d7cf4770a1e09ccfcc748cf3f42
Olena Novitska <109989618+OlenaNov@users.noreply.github.com>
2023-02-27 13:54:30 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+// import SimpleLightbox from "simplelightbox";
+// import "simplelightbox/dist/simple-lightbox.min.css";
 
 import markupImage from './markup-image-card';
 import getImages from './fetchImages';
